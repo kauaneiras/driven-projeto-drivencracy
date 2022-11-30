6daf5eae3e4b64a434f3a6f79b7662b4b5f0fb3a
Kauan T. Eiras <43351064+kauaneiras@users.noreply.github.com>
2022-11-30 11:28:55 -0300

    fix catch controller

--- src/controllers/poll.controller.js
+++ src/controllers/poll.controller.js
@@ -2,10 +2,7 @@ import db from "../database/db.js";
 import dayjs from "dayjs";
 
 async function PostPollController (req, res) {
-    // {
-    //     title: "Qual a sua linguagem favorita?",
-    //         expireAt: "2022-02-28 01:00" 
-    // }
+   
     const { title, expireAt } = req.body;
     const poll = { title, expireAt };
 
@@ -17,13 +14,17 @@ async function PostPollController (req, res) {
         await db.collection('polls').insertOne(poll);
         res.status(201).send([poll]);
     }catch(err){
-        console.log(err);
+        res.status(500).send(err);
     }
 }
 
 async function GetPollController (req, res) {
-    const polls = await db.collection('polls').find({}).toArray();
-    res.send(polls);
+    try{
+        const polls = await db.collection('polls').find({}).toArray();
+        res.send(polls);
+    }catch(err){
+        res.status(500).send(err);
+        }
 }
 
 export { PostPollController, GetPollController };
